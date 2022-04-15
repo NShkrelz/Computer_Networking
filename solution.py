b076bd9fb0ace9918656e2312a580ed51fa6d850
NShkrelz <96559206+NShkrelz@users.noreply.github.com>
2022-04-15 17:47:46 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -125,18 +125,18 @@ def ping(host, timeout=1):
 
     packet_min = min(varValues)
     vars.append(packet_min)
-    print("min = ", packet_min)
+    print("min = ", round(packet_min,2))
     packet_avg = sum(varValues)/len(varValues)
     vars.append(packet_avg)
-    print("avg = ", packet_avg)
+    print("avg = ", round(packet_avg, 2))
     packet_max = max(varValues)
     vars.append(packet_max)
-    print("max = ", packet_max)
+    print("max = ", round(packet_max,2))
     stdev_var = statistics.stdev(varValues)
     vars.append(stdev_var)
-    print("stddev = ", stdev_var)
+    print("stddev = ", round(stdev_var,2))
     #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
-    #print("round-trip min/avg/max/stddev = ", vars[0], "/", vars[1], "/", vars[2], "/", vars[3], " ms")
+    print("round-trip min/avg/max/stddev = ", vars[0], "/", vars[1], "/", vars[2], "/", vars[3], " ms")
     return vars
 
 if __name__ == '__main__':
