fd15ffbd51bc3275731416b7bb992a358af6099e
NShkrelz <96559206+NShkrelz@users.noreply.github.com>
2022-04-14 21:09:10 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -129,8 +129,7 @@ def ping(host, timeout=1):
     vars.append(packet_max)
     stdev_var = statistics.stdev(varValues)
     vars.append(stdev_var)
-    vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),
-            str(stdev_var)]
+    #vars = [str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)), str(stdev_var)]
     print("round-trip min/avg/max/stddev = ",round(packet_min, 2),"/",(round(packet_avg, 2),"/",(round(packet_max, 2),round(stdev_var,2)," ms")
     return vars
 
